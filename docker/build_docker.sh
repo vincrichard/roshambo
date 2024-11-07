8c42ac54972d0bf95fc2ba4e4114e96cecd839fc
vincrichard <vincent.richard@elix-inc.com>
2024-11-07 22:42:37 +0900

    [Add] support for CUDA version

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,4 +1,1 @@
-docker build \                                                      
-  --build-arg USER_ID=$(id -u) \
-  --build-arg GROUP_ID=$(id -g) \
-  -t roshambo .
+docker build --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg CUDA_VERSION="12.5.0" -t roshambo .
